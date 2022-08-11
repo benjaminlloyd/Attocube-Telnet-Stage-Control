8455baabe24673e2c8d9fabf2b364d339e456bc5
Benjamin Lloyd <61776613+benjaminlloyd@users.noreply.github.com>
2022-08-11 14:14:43 -0400

    Update Example.py

--- Example.py
+++ Example.py
@@ -3,4 +3,6 @@ from Connect import Connect
 
 stages=Control(Connect('192.168.7.10').tn)
 stages.turnon()
+stages.roomtemp()
+stages.move(3,-1,20,280) #axis, steps, voltage, frequency
 stages.turnoff()
