679885384e969ca75dfe86eb10a013c8ac9b6992
Benjamin Lloyd <thereallloyd@gmail.com>
2022-08-19 09:28:04 -0400

    Update Example.py

--- Example.py
+++ Example.py
@@ -5,4 +5,5 @@ stages=Control(Connect('192.168.7.10').tn)
 stages.turnon()
 stages.roomtemp()
 stages.move(3,-1,20,280) #axis, steps, voltage, frequency
+stages.move(3,2) #axis and steps are necessary, voltage/frequency are optional
 stages.turnoff()
